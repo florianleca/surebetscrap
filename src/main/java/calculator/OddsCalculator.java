81c33d292aaa56a88c7fd2d4f5963696ccb55670
Florian Leca <florianleca@hotmail.fr>
2023-02-25 19:40:52 +0100

    adding some debug and testing tools

--- src/main/java/calculator/OddsCalculator.java
+++ src/main/java/calculator/OddsCalculator.java
@@ -6,6 +6,7 @@ public class OddsCalculator {
 	}
 
 	public static boolean isASureBet(double c1, double c2, double c3) {
+		System.out.println((1 / c1) + (1 / c2) + (1 / c3));
 		return (1 / c1) + (1 / c2) + (1 / c3) < 1;
 	}
 
@@ -37,5 +38,13 @@ public class OddsCalculator {
 		System.out.println("Si l'issue 1 est gagnante, je touche " + a * c1);
 		System.out.println("Si l'issue 2 est gagnante, je touche " + b * c2);
 		System.out.println("Le gain net est donc de " + (a * c1 - totalBetAmount));
+	}
+	public static void main(String[] args) {
+		System.out.println(OddsCalculator.isASureBet(2.25, 3.25, 3.1));
+		System.out.println(OddsCalculator.isASureBet(2.25, 4.5, 2.15));
+		System.out.println(OddsCalculator.isASureBet(2.45, 4.8, 2.1));
+		System.out.println(OddsCalculator.isASureBet(2.45, 3.85, 2.35));
+		System.out.println(OddsCalculator.isASureBet(5, 2.82, 1.66));
+		System.out.println(OddsCalculator.isASureBet(5, 3.57, 1.5));
 	}
 }
--- src/main/java/common/Main.java
+++ src/main/java/common/Main.java
@@ -1,6 +1,7 @@
 package common;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import org.jsoup.Jsoup;
@@ -20,6 +21,19 @@ public class Main {
 		BetclicScrapTools bst = new BetclicScrapTools();
 		Document doc = Jsoup.connect("https://www.betclic.fr/football-s1/ligue-1-uber-eats-c4").get();
 		String data = doc.data();
+		
+		Document doc2 = Jsoup.connect("https://www.winamax.fr/paris-sportifs/sports/100000").get();
+		String data2 = doc2.data();
+		
+		Document doc3 = Jsoup.connect("https://www.winamax.fr/paris-sportifs/calendar").get();
+		String data3 = doc3.data();
+		
+		Document doc4 = Jsoup.connect("https://www.vbet.fr/calendrier").get();
+		String data4 = doc4.data();
+		
+		PrintWriter writer = new PrintWriter("mon-fichier.txt", "UTF-8");
+		writer.println(data4);
+		writer.close();
 		
 		data = bst.dataClean(data);
 		List<String> isolatedMatchesData = bst.isolateMatchesData(data);
